15692bc1966900448373f3661a6aff51f1ae1265
dmitry-sidorov <jimmaggot@mail.ru>
2025-07-16 14:57:55 +0300

    Add pool

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -2,6 +2,7 @@ defmodule Todo.Database do
   use GenServer
 
   @db_folder "./persist"
+  @worker_ids [0, 1, 2]
 
   def start do
     GenServer.start(__MODULE__, nil, name: __MODULE__)
@@ -11,13 +12,27 @@ defmodule Todo.Database do
     GenServer.cast(__MODULE__, {:store, key, data})
   end
 
+  @spec get(any()) :: any()
   def get(key) do
     GenServer.call(__MODULE__, {:get, key})
   end
 
   def init(_) do
-    File.mkdir_p!(@db_folder)
-    {:ok, nil}
+    workers_pool = @worker_ids
+    |> Enum.reduce(%{}, fn (index, acc) ->
+      {:ok, pid} = Todo.DatabaseWorker.start(@db_folder)
+      Map.put(acc, index, pid)
+    end)
+
+    {:ok, workers_pool}
+  end
+
+  def show_pool() do
+    GenServer.call(__MODULE__, :pool)
+  end
+
+  def handle_call(:pool, _, state) do
+    {:reply, state, state}
   end
 
   def handle_cast({:store, key, data}, state) do
