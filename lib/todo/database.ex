7a7624f61ac3b8478690c5052efea803ea86ffe6
dmitry-sidorov <jimmaggot@mail.ru>
2026-02-04 15:56:53 +0300

    Refactoring: implement database server using supervisor

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -1,45 +1,18 @@
 defmodule Todo.Database do
-  use GenServer
-
+  @pool_size 3
   @db_folder "./persist"
 
   def start_link(_) do
-    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
-  end
-
-  def store(key, data) do
-    key
-    |> choose_worker()
-    |> Todo.DatabaseWorker.store(key, data)
-  end
-
-  def get(key) do
-    key
-    |> choose_worker()
-    |> Todo.DatabaseWorker.get(key)
-  end
-
-  defp choose_worker(key) do
-    GenServer.call(__MODULE__, {:choose_worker, key})
-  end
-
-  @impl GenServer
-  def init(_) do
-    IO.puts("Starting database server.")
+    IO.puts("Starting database supervisor.")
     File.mkdir_p!(@db_folder)
-    {:ok, start_workers()}
+
+    children = 1..@pool_size |> Enum.map(&worker_spec/1)
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 
-  @impl GenServer
-  def handle_call({:choose_worker, key}, _, workers) do
-    worker_key = :erlang.phash2(key, 3)
-    {:reply, Map.get(workers, worker_key), workers}
-  end
+  defp worker_spec(worker_id) do
+    default_worker_spec = {Todo.DatabaseWorker, {@db_folder, worker_id}}
 
-  defp start_workers() do
-    for index <- 1..3, into: %{} do
-      {:ok, pid} = Todo.DatabaseWorker.start_link(@db_folder, index)
-      {index - 1, pid}
-    end
+    Supervisor.child_spec(default_worker_spec, id: worker_id)
   end
 end
