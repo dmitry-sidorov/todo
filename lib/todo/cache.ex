d56259bcc9b1e390bc6f75265a4db54996981840
dmitry-sidorov <jimmaggot@mail.ru>
2026-02-11 20:29:38 +0300

    Add dynamic supervisor for cache process

--- lib/todo/cache.ex
+++ lib/todo/cache.ex
@@ -1,21 +1,37 @@
 defmodule Todo.Cache do
-  use GenServer
 
   def start_link(_) do
-    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+    IO.puts("Starting to-do cache.")
+
+    DynamicSupervisor.start_link(
+      name: __MODULE__,
+      strategy: :one_for_one
+    )
+  end
+
+  defp start_child(todo_list_name) do
+    DynamicSupervisor.start_child(
+      __MODULE__,
+      {Todo.Server, todo_list_name}
+    )
+  end
+
+  def child_spec(_arg) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, []},
+      type: :supervisor
+    }
   end
 
   def server_process(todo_list_name) do
-    GenServer.call(__MODULE__, {:server_process, todo_list_name})
+    case start_child(todo_list_name) do
+      {:ok, pid} -> pid
+      {:error, {:already_started, pid}} -> pid
+    end
   end
 
-  @impl GenServer
-  def init(_) do
-    IO.puts("Starting to-do cache.")
-    {:ok, %{}}
-  end
 
-  @impl GenServer
   def handle_call({:server_process, todo_list_name}, _, todo_servers) do
     case Map.fetch(todo_servers, todo_list_name) do
       {:ok, todo_server} ->
